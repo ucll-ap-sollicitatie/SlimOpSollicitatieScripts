07ddc45db62295cdfda82d6f5244c715077c7ec0
Jonathan Peypops <jonathan.peypops@student.ucll.be>
2021-02-25 16:19:22 +0100

    leesbaarheid

--- videoEdit.py
+++ videoEdit.py
@@ -1,55 +1,51 @@
-# Import everything needed to edit video clips
-#from moviepy.editor import *
-
-# loading video dsa gfg intro video
-#clip = VideoFileClip("TEST.mp4")
-
-# clipping of the video
-# getting video for only starting 10 seconds
-#clip = clip.subclip(0, 1)
-
-# Reduce the audio volume (volume x 0.8)
-#clip = clip.volumex(0.8)
-
-# Generate a text clip
-#txt_clip = TextClip("DICKS", fontsize=75, color='black')
-
-# setting position of text in the center and duration will be 10 seconds
-#txt_clip = txt_clip.set_pos('center').set_duration(10)
-
-# Overlay the text clip on the first video clip
-#video = CompositeVideoClip([clip, txt_clip])
-
-# showing video
-#video.ipython_display(width=280)
-
 import cv2
+import sys
+import textwrap
 
-cap = cv2.VideoCapture('Test.mp4')
-out = cv2.VideoWriter('output.mp4', -1, 20.0, (640, 480))
+videoName = sys.argv[1]
+file = open(sys.argv[2], 'r')
+lines = file.readlines()
+endTime = []
+textOnScreen = []
+if len(lines) > 2:
+    lines.pop(0)
+    for x in range(0, len(lines)):
+        if x % 2 == 0:
+            endTime.append(float(lines[x].strip('\n')))
+        else:
+            textOnScreen.append(lines[x].strip('\n'))
+
+cap = cv2.VideoCapture(videoName)
 fps = cap.get(cv2.CAP_PROP_FPS)
+out = cv2.VideoWriter('processed_' + videoName, -1, fps, (640, 480))
 frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 n_frames = 0
-startTime = 0
-endTime = [1, 2]
 timeCounter = 0
-textOnScreen = ["TEXT 1", "TEXT 2"]
-
+cx, cy = 0, 375
+#print(float(frame_count) / float(fps))
+#print(fps, frame_count)
+#print(len(endTime))
 
 while True:
     ret, frame = cap.read()
     n_frames += 1
     duration = n_frames / float(fps)
-    font = cv2.FONT_HERSHEY_PLAIN
-    if startTime - 1 <= duration <= endTime[timeCounter]:
-        cv2.putText(frame, textOnScreen[timeCounter], (50, 50), font, 1, (0, 255, 255), 2, cv2.LINE_4)
+    font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+    if n_frames - 1 <= round(endTime[timeCounter] * fps):
+        #print('END TIME: ' + str(endTime[timeCounter]))
+        #print("DURATION: " + str(duration))
+        wrapped = textwrap.wrap(textOnScreen[timeCounter], 40)
+        cv2.rectangle(frame, (0, 480), (640, 350), (255, 255, 255), -2)
+        for he, line in enumerate(wrapped):
+            gap = 20
+            cv2.putText(frame, line, (cx, cy + he * gap), font, 1, (235, 144, 65), 1, cv2.LINE_AA)
+
+            if duration >= endTime[timeCounter]:
+                timeCounter += 1
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
         out.write(frame)
         cv2.imshow('video', frame)
-        if duration == endTime[timeCounter]:
-            startTime = endTime[timeCounter]
-            timeCounter += 1
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    if duration == float(frame_count)/ float(fps):
+    if duration == float(frame_count) / float(fps):
         cap.release()
         cv2.destroyWindow()
